2858181db4a66507e2cdcd7a5742798225db71ef
Ferdinand Schlatt <fschlatt@gmail.com>
2020-03-13 23:27:55 +0100

    add evaluator errors

--- pyker/error.py
+++ pyker/error.py
@@ -4,5 +4,11 @@ class InvalidRankError(Exception):
 class InvalidSuitError(Exception):
     pass
 
-class InvalidOrder(Exception):
+class InvalidOrderError(Exception):
+    pass
+
+class InvalidHandSizeError(Exception):
+    pass
+
+class InvalidHandRankError(Exception):
     pass
