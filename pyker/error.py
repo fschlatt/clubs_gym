d4b6893f68d3157188a255c6b77dd8acbbadc4b5
Ferdinand Schlatt <fschlatt@gmail.com>
2020-03-25 10:17:13 +0100

    add class spacing

--- pyker/error.py
+++ pyker/error.py
@@ -1,23 +1,30 @@
 class InvalidRankError(Exception):
     pass
 
+
 class InvalidSuitError(Exception):
     pass
+
 
 class InvalidOrderError(Exception):
     pass
 
+
 class InvalidHandSizeError(Exception):
     pass
+
 
 class InvalidHandRankError(Exception):
     pass
 
+
 class TooFewActivePlayersError(Exception):
     pass
 
+
 class TableResetError(Exception):
     pass
+
 
 class UnknownRenderModeError(Exception):
     pass
