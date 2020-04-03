e433b12e09a07f4fea993f7070126ded54636339
Ferdinand Schlatt <fschlatt@gmail.com>
2020-04-03 10:09:19 +0200

    add invalid init test

--- test/test_poker/test_card.py
+++ test/test_poker/test_card.py
@@ -40,3 +40,12 @@ def test_trick():
     cards = deck.draw(2)
     assert cards[0] != poker.Card('Ah')
     assert cards[1] != poker.Card('2s')
+
+
+def test_invalid_init():
+    with pytest.raises(error.InvalidRankError):
+        poker.Card('1s')
+        poker.Card('1t')
+
+    with pytest.raises(error.InvalidSuitError):
+        poker.Card('At')
