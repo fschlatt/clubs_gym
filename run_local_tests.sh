e172c27d6227aa090ab29f4c5af11c0820207f98
Ferdinand Schlatt <fschlatt@gmail.com>
2020-05-03 17:29:03 +0200

    update test script

--- run_local_tests.sh
+++ run_local_tests.sh
@@ -1,4 +1,6 @@
-flake8 clubs --count --select=E9,F63,F7,F82 --show-source --statistics
-flake8 clubs --count --exit-zero --max-complexity=10 --max-line-length=88 --statistics
-mypy clubs --config-file=mypy.ini
-pytest --cov clubs --cov-report html test
+#!/usr/bin/env bash
+
+flake8 clubs/ --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 clubs/ --count --exit-zero --max-complexity=10 --max-line-length=88 --statistics
+mypy clubs/ --config-file=mypy.ini
+pytest --cov clubs/ --cov-report html test/
