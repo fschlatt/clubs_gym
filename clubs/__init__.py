1b08a68f25fe8d04e3ca474b0e66b4ab098cf8c7
Ferdinand Schlatt <fschlatt@gmail.com>
2020-10-13 10:09:39 +0200

    bump version

--- clubs/__init__.py
+++ clubs/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ["configs", "poker"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Ferdinand Schlatt"
-__license__ = "GLP-3.0"
+__license__ = "GPL-3.0"
 __copyright__ = f"Copyright (c) 2020, {__author__}."
 __homepage__ = "https://github.com/fschlatt/clubs"
 __docs__ = (
