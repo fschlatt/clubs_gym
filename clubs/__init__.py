cac50f2ab1516c83d8ae5bc9f11877d52f2adec5
Ferdinand Schlatt <fschlatt@gmail.com>
2020-10-13 18:22:26 +0200

    reset version

--- clubs/__init__.py
+++ clubs/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["configs", "poker"]
-__version__ = "0.1.1"
+__version__ = "0.1.0"
 __author__ = "Ferdinand Schlatt"
 __license__ = "GPL-3.0"
 __copyright__ = f"Copyright (c) 2020, {__author__}."
