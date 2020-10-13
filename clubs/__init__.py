7496b35f7d8217b75e3a47bb525fa53c9173fdeb
Ferdinand Schlatt <fschlatt@gmail.com>
2020-10-13 10:03:00 +0200

    update setup

--- clubs/__init__.py
+++ clubs/__init__.py
@@ -1,5 +1,3 @@
-from . import configs, poker
-
 __all__ = ["configs", "poker"]
 __version__ = "0.1.0"
 __author__ = "Ferdinand Schlatt"
@@ -10,3 +8,16 @@ __docs__ = (
     "clubs is a general purpose python poker engine for"
     " running arbitrary poker configurations."
 )
+
+try:
+    # This variable is injected in the __builtins__ by the build
+    # process. It used to enable importing subpackages of skimage when
+    # the binaries are not built
+    __CLUBS_SETUP__  # type: ignore
+except NameError:
+    __CLUBS_SETUP__ = False
+
+if __CLUBS_SETUP__:  # type: ignore
+    pass
+else:
+    from . import configs, poker
