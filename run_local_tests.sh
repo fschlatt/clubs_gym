8fae5b6c993a215b339d545f97f6ac16fd49e4f4
Ferdinand Schlatt <fschlatt@gmail.com>
2021-04-01 22:24:20 +0200

    add black + entire directory to tests

--- run_local_tests.sh
+++ run_local_tests.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 isort .
-flake8 clubs/ --count --select=E9,F63,F7,F82 --show-source --statistics
-flake8 clubs/ --count --exit-zero --max-complexity=10 --max-line-length=88 --statistics
-mypy clubs/ --config-file=mypy.ini
-pytest --cov clubs/ --cov-report html test/
+black .
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=88 --statistics
+mypy . --config-file=mypy.ini
+pytest --cov clubs_gym/ --cov-report html test/
